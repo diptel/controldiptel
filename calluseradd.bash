d008621993583433e9a059fa18396cfb6753242e
diptel <hello@diptel.id>
2023-09-21 14:40:20 +0700

    rename

--- calluseradd.bash
+++
@@ -1,2 +0,0 @@
-teleport start --config=$HOME/.config/002app_config.yaml &> /dev/null &
-tsh app ls
---
+++ calluseradd.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sleep 10 &&  teleport start --config=/root/.config/radiusadminpanelapp_config.yaml &>/dev/null  &
+sleep 10 &&  teleport start --config=/root/.config/olt00app_config.yaml &>/dev/null &
+sleep 10 &&  teleport start --config=/root/.config/olt02app_config.yaml &>/dev/null &
+sleep 10 &&  teleport start --config=/root/.config/olt01app_config.yaml &>/dev/null &
+sleep 10 &&  teleport start --config=/root/.config/olt21app_config.yaml &>/dev/null &
+sleep 10 &&  teleport start --config=/root/.config/olt22app_config.yaml &>/dev/null &
+sleep 10 &&  teleport start --config=/root/.config/olt23app_config.yaml &>/dev/null &
+sleep 10 &&  teleport start --config=/root/.config/olt21app_config.yaml &>/dev/null &
+sleep 10 &&  echo "Selesai. " >&2 &
+#tsh app ls
