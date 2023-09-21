ea275595ae37ae6df9d5f07d14a77649a680e48d
diptel <hello@diptel.id>
2023-09-21 14:44:46 +0700

    rename replace

--- runteleportapp.sh
+++ runteleportapp.sh
@@ -9,3 +9,6 @@ sleep 10 &&  teleport start --config=/root/.config/olt23app_config.yaml &>/dev/n
 sleep 10 &&  teleport start --config=/root/.config/olt21app_config.yaml &>/dev/null &
 sleep 10 &&  echo "Selesai. " >&2 &
 #tsh app ls
+# Instalasi awal $ chmod a+x  runteleportapp.sh
+# crontab otomatis $ sudo crontab -e 
+# tambahkan satu baris  @reboot  runteleportapp.sh
